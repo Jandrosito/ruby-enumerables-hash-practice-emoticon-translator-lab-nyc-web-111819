9a520a9f413d15436de5b308f3370cbd4a644dd1
Edward Castillo <castilloedwarda@gmail.com>
2019-11-15 05:58:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,36 +1,37 @@
 # require modules here
 
-  def load_library(emoticons)
-    emoticons = YAML.load_file('./lib/emoticons.yml')
-    final_hash={
-      get_meaning: {},
-      get_emoticon: {}
-    }
-  emoticons.each do |meaning, english_japanese_emoji|
-final_hash[:get_emoticon][english_japanese_emoji[0]] = english_japanese_emoji[1]
-final_hash[:get_meaning][english_japanese_emoji[1]] = meaning
+def load_library
+  # code goes here
+def load_library(asdf)
+  emoticons = YAML.load_file(asdf)
+  new_hash = {}
+  new_hash[:get_meaning] = {}
+  new_hash[:get_emoticon] = {}
+
+  emoticons.map {|ele, subele|
+    new_hash[:get_meaning][subele[1]] = ele
+    new_hash[:get_emoticon][subele[0]] = subele[1]
+  }
+  new_hash
+end
+
+def get_japanese_emoticon
+  # code goes here
+def get_japanese_emoticon(file, emoticon)
+  if !!load_library(file)[:get_emoticon][emoticon]
+    return load_library(file)[:get_emoticon][emoticon]
+  else
+    return "Sorry, that emoticon was not found"
   end
- final_hash
+
 end
 
-def get_japanese_emoticon(emoticons, target)
-  new_hash=load_library(emoticons)
-  emoticons = YAML.load_file('./lib/emoticons.yml')
-  new_hash[:get_emoticon].each do |english, japan|
- if english == target
-   return japan
- end
- end
- return "Sorry, that emoticon was not found"
-end
+def get_english_meaning
+  # code goes here
+def get_english_meaning(file, emoticon)
+    if !!load_library(file)[:get_meaning][emoticon]
+      return load_library(file)[:get_meaning][emoticon]
+    else
+      return "Sorry, that emoticon was not found"
+  end
 
-def get_english_meaning(emoticons, target0)
-  library=load_library(emoticons)
-  emoticons = YAML.load_file('./lib/emoticons.yml')
-    library[:get_meaning].each do |k,v|
-      if k == target
-        return v
-      end
-    end
-    return  "Sorry, that emoticon was not found"
-end
