81a29386824ede7a067dc4ffacc0667d31b278fb
Edward Castillo <castilloedwarda@gmail.com>
2019-11-15 05:50:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,11 +2,21 @@
 emoticons = YAML.load_file('./lib/emoticons.yml')
 
 def load_library
-  # code goes here
+  def load_library(emoticons)
+    emoticons = YAML.load_file('./lib/emoticons.yml')
+    final_hash={
+      get_meaning: {},
+      get_emoticon: {}
+    }
+  emoticons.each do |menaing, eglish_japanaise_emoji|
+final_hash[:get_emoticon][eglish_japanaise_emoji[0]] = eglish_japanaise_emoji[1]
+final_hash[:get_meaning][eglish_japanaise_emoji[1]] = menaing
+  end
+ final_hash
 end
 
 def get_japanese_emoticon
-  # code goes here
+  
 end
 
 def get_english_meaning
